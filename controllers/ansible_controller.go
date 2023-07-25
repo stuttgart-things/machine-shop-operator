8b46524e98815d3ef60d3b33e5bc9dce6b32b995
Patrick Hermann <patrick.hermann@sva.de>
2023-07-25 05:41:41 +0000

    updated machine-shop-operator 23.0725.0541 for tag version v0.1.108-ANSIBLE-CR

--- controllers/ansible_controller.go
+++ controllers/ansible_controller.go
@@ -46,10 +46,9 @@ var (
 		"playbook":  "cm",
 		"job":       "job",
 	}
-	maxResourceCheckRetries   = 10
-	intervalResourceChecks, _ = time.ParseDuration("5s")
-	ansibleJobNamespace       = os.Getenv("ANSIBLE_JOB_NAMESPACE")
-	redisStream               = os.Getenv("REDIS_STREAM")
+	maxResourceCheckRetries = 10
+	ansibleJobNamespace     = os.Getenv("ANSIBLE_JOB_NAMESPACE")
+	redisStream             = os.Getenv("REDIS_STREAM")
 )
 
 // AnsibleReconciler reconciles a Ansible object
@@ -137,7 +136,7 @@ func (r *AnsibleReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 	// CHECK FOR VALUES IN REDIS
 	try := 0
-	for range time.Tick(time.Second * intervalResourceChecks) {
+	for range time.Tick(time.Second * 5) {
 
 		if try <= maxResourceCheckRetries {
 
